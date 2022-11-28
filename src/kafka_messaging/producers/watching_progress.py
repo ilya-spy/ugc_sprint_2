3ddb479c6a0d8a879d93ac14f37a78a4d315a537
b.danny.v <bdannyv@gmail.com>
2022-11-28 09:51:57 +0400

    watching progress consumer

---
+++ src/kafka_messaging/producers/watching_progress.py
@@ -0,0 +1,17 @@
+from kafka_messaging.producers.base import BaseProducer
+import uuid
+
+
+class WatchingProgressProducer(BaseProducer):
+    @property
+    def topic_name(self) -> str:
+        return 'user_watching_progress'
+
+    def __init__(self):
+        super(WatchingProgressProducer, self).__init__()
+
+    def send(self, progress: str, user_id: uuid.UUID, movie_id: uuid.UUID, *args, **send_kwargs):
+        super(WatchingProgressProducer, self).send(
+            value=progress.encode(encoding='utf-8'),
+            key=f'{user_id}_{movie_id}'.encode(encoding='utf-8')
+        )

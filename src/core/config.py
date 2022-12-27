1b4af268129a7ed7f2832fe7953c03343f9beda2
b.danny.v <bdannyv@gmail.com>
2022-12-27 23:49:31 +0400

    gate config moved to common module

--- src/core/config.py
+++ src/core/config.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from pydantic import BaseSettings, Field
 
 from db.clickhouse.ch_config import ClickHouseConfig
@@ -11,16 +13,24 @@ class SentryConfig(BaseSettings):
         env_prefix = "sentry_"
 
 
+class AuthAPIConfig(BaseSettings):
+    address: str = Field(default="http://auth_api:8000/")
+
+    class Config:
+        env_prefix = "auth_api_"
+
+
 class Config(BaseSettings):
     """Настройки приложения."""
 
-    app_name: str = Field(default="ugc_services")
+    app_name: str = Field(default="ugc_gate")
     app_config: str = Field(default="dev")
     debug: bool = Field(default=True)
     loglevel: str = Field(default="DEBUG")
 
     olap: ClickHouseConfig = ClickHouseConfig()
     kafka: KafkaConfig = KafkaConfig()
+    auth_api: AuthAPIConfig = AuthAPIConfig()
     sentry: SentryConfig = SentryConfig()
 
 
@@ -40,7 +50,7 @@ class DevelopmentConfig(Config):
 # Choose default config
 app_config = Config().app_config
 
-config: ProductionConfig | DevelopmentConfig
+config: Union[ProductionConfig, DevelopmentConfig]
 if app_config == "prod":
     config = ProductionConfig()
 if app_config == "dev":
